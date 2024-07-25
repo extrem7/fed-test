658f2a56b6241469695bc0d2164c41084fcc3236
urix <extremtmnt@gmail.com>
2024-07-25 17:59:41 +0300

    chore: repair tailwind in ts support

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,25 @@
 import antfu from '@antfu/eslint-config'
 import eslintPluginReadableTailwind from 'eslint-plugin-readable-tailwind'
+import tailwind from 'eslint-plugin-tailwindcss'
 
-export default antfu({
-  vue: true,
-}, {
-  plugins: {
-    'readable-tailwind': eslintPluginReadableTailwind,
-  },
-  rules: {
+export default antfu({}, [
+  ...tailwind.configs['flat/recommended'],
+  {
+    plugins: {
+      'readable-tailwind': eslintPluginReadableTailwind,
+    },
+    rules: {
     // enable all recommended rules to warn
-    ...eslintPluginReadableTailwind.configs.warning.rules,
-    // enable all recommended rules to error
-    ...eslintPluginReadableTailwind.configs.error.rules,
+      ...eslintPluginReadableTailwind.configs.warning.rules,
+      // enable all recommended rules to error
+      ...eslintPluginReadableTailwind.configs.error.rules,
 
-    // or configure rules individually
-    'readable-tailwind/multiline': ['warn', { printWidth: 80 }],
+      // or configure rules individually
+      'readable-tailwind/multiline': ['warn', { printWidth: 80 }],
+      'tailwindcss/classnames-order': 'off',
+    },
+    settings: {
+      tailwindcss: { callees: ['classnames', 'clsx', 'ctl', 'cva', 'cn'] },
+    },
   },
-})
+])
